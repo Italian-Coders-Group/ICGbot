5872f662fb8dbd345272e8b1fa6a9f98c138faf5
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-09-27 18:10:14 +0200

    correctly make embed from traceback

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import discord
 import traceback
+import io
 from typing import Union
 
 from discord import Message, Embed
@@ -21,14 +22,27 @@ def embed(title: str, content: str, color: discord.Color) -> Embed:
 	return data
 
 
-def getTraceback(tb) -> str:
-	return traceback.format_exception( type(tb), value={tb.__cause__, tb.__context__, tb.__suppress_context__}, tb=tb )
+def getColor(RGB: str) -> discord.Color:
+	rgb = RGB.split(',')
+	r: int = int( rgb[0] )
+	g: int = int( rgb[1] )
+	b: int = int( rgb[2] )
+	return discord.colour.Color.from_rgb(r, g, b)
 
 
-def getTracebackEmbed(tb: Exception) -> Embed:
+def getTraceback(exc: Exception) -> str:
+	buf = io.StringIO()
+	buf.write('```')
+	traceback.print_tb(exc.__traceback__, None, buf)
+	buf.write('```')
+	print(buf.getvalue())
+	return buf.getvalue()
+
+
+def getTracebackEmbed(exc: Exception) -> Embed:
 	return embed(
 		title='Uncaught Exception!',
-		content=getTraceback(tb),
+		content=getTraceback(exc),
 		color=discord.Color.red()
 	)
 
